791e1bae1aff1bab33b9ec7239801dd107335c18
giacomo-spadoni <giaky.spado@gmail.com>
2024-05-06 15:31:20 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,128 @@
+// creo le variabili che selezionano gli elementi HTML
+const griglia = document.getElementById('grid')
+const facile = document.getElementById('easy')
+const medio = document.getElementById('standard')
+const difficile = document.getElementById('hard')
+const fine = document.getElementById('end')
+const score = document.getElementById('score')
+
+let f = 0
+
+let gameover = false
+
+let punteggio = 0
+livescore(punteggio)
+
+const bombe = []
+
+
+
+// creo l'evento al tasto fine partita
+
+fine.addEventListener('click', function () {
+    show()
+
+    griglia.classList = ''
+    griglia.innerHTML = ''
+    bombe.length = 0
+    punteggio = 0
+    livescore(punteggio)
+    gameover = false
+})
+
+// creo gli eventi ai tasti di difficoltà diverse
+
+facile.addEventListener('click', start.bind('null', 'easy', 100))
+
+medio.addEventListener('click', start.bind('null', 'standard', 81))
+
+difficile.addEventListener('click', start.bind('null', 'hard', 49))
+
+// creo la funzione che fa iniziare la partita
+
+function start(livello, celle) {
+    hidden()
+
+    f = celle
+
+    let n = 0
+    while (n < 16) {
+        let z = Math.floor(Math.random() * f)
+        if (!bombe.includes(z)) {
+            bombe.push(z)
+            n++
+        }
+    }
+
+    griglia.classList.add(livello)
+
+    for (let i = 0; i < f; i++) {
+        addQuadrato(griglia, i)
+    }
+}
+
+// creo la funzione che aggiunge i quadrati
+
+function addQuadrato(griglia, i) {
+    let quadrato = document.createElement('div')
+    quadrato.classList.add('square')
+    // funzione dentro funzione senza .bind
+    quadrato.addEventListener('click', function () {
+        addClick(quadrato, i)
+    })
+    // quadrato.addEventListener('click', addClick.bind('null', quadrato, i))
+    griglia.append(quadrato)
+}
+
+// creo l'evento per il click sulle celle
+
+function addClick(quadrato, i) {
+    if (!quadrato.classList.contains('evidenziato') && gameover == false) {
+        quadrato.classList.add('evidenziato')
+
+        // let x = Math.floor(Math.random() * 10) + 1
+        if (quadrato.classList.contains('evidenziato')) {
+            if (bombe.includes(i)) {
+                quadrato.innerHTML = '<i class="fa-solid fa-bomb"></i>'
+                lose.classList.remove('hidden')
+                gameover = true
+            } else {
+                punteggio++
+                livescore(punteggio)
+                if (punteggio == f - 16) {
+                    win.classList.remove('hidden')
+                    gameover = true
+                }
+            }
+        } else {
+            quadrato.innerHTML = ''
+        }
+    }
+}
+
+// creo la funzione che mostra i tasti dei livelli
+
+function show() {
+    facile.classList.remove('hidden')
+    medio.classList.remove('hidden')
+    difficile.classList.remove('hidden')
+    score.classList.add('hidden')
+    fine.classList.add('hidden')
+    lose.classList.add('hidden')
+    win.classList.add('hidden')
+}
+
+// creo la funzione che nasconde i tasti dei livelli
+
+function hidden() {
+    facile.classList.add('hidden')
+    medio.classList.add('hidden')
+    difficile.classList.add('hidden')
+
+    score.classList.remove('hidden')
+    fine.classList.remove('hidden')
+}
+
+function livescore(punteggio) {
+    score.innerHTML = "Il tuo punteggio è: " + punteggio
+}
